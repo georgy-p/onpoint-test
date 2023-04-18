da140f021d806c34b4e67a4cac0b6c64f16a36cf
George <81578359+georgy-p@users.noreply.github.com>
2023-04-18 21:17:24 +0300

    create advantages list

---
+++ src/texts/advantagesList.js
@@ -0,0 +1,12 @@
+export default {
+  list1: [
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    'Faucibus pulvinar elementum integer enim',
+    'Faucibus pulvinar elementum integer enim',
+  ],
+  list2: [
+    'Mi bibendum neque egestas congue quisque egestas diam',
+    'Venenatis lectus magna fringilla urna',
+    'Venenatis lectus magna fringilla urna',
+  ],
+};
