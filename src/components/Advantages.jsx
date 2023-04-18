490d497f3379e9131422d134c1c4bd702832d29e
George <81578359+georgy-p@users.noreply.github.com>
2023-04-18 21:15:14 +0300

    create Advantages slide component

---
+++ src/components/Advantages.jsx
@@ -0,0 +1,56 @@
+import prev from '../images/product_page/prev.png';
+import next from '../images/product_page/next.png';
+import advantagesList from '../texts/advantagesList';
+import { useState } from 'react';
+
+const Advantages = () => {
+  const { list1, list2 } = advantagesList;
+  const [isNext, setNext] = useState(false);
+
+  const handlePrev = () => setNext(!isNext);
+  const handleNext = () => setNext(!isNext);
+
+  const renderItem = (text, number) => (
+    <div className='list-item-group' key={number}>
+      <p className='list-item-number'>{number}</p>
+      <p className='list-item-text'>{text}</p>
+    </div>
+  );
+
+  const currentList = isNext ? list2 : list1;
+
+  const blockClick = isNext ? 'active' : '';
+
+  return (
+    <div className='advantages-container'>
+      <div className='advantages-inner-container'>
+        <div className='col-1'>
+          <p className='advantages-header'>ПРЕИМУЩЕСТВА</p>
+          <h1 className='advantages-subheader'>
+            BREND<span>XY</span>
+          </h1>
+          <div className='list-container'>
+            {currentList.map((text, index) => {
+              const adder = isNext ? 4 : 1;
+              const number = `0${index + adder}`;
+              return renderItem(text, number);
+            })}
+          </div>
+          <div className='list-btn-group'>
+            <div className='list-btn' onClick={handlePrev}>
+              <img src={prev} alt='prev' />
+            </div>
+            <div className='list-circle active' />
+            <div className={`list-circle ${blockClick}`} />
+            <div className='list-btn' onClick={handleNext}>
+              <img src={next} alt='next' />
+            </div>
+          </div>
+        </div>
+        <div className='col-2'>x</div>
+      </div>
+    </div>
+  );
+};
+
+export default Advantages;
