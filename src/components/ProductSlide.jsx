c1b945cd5e6590d96a0760e5bfcb035e7072196b
George <81578359+georgy-p@users.noreply.github.com>
2023-04-19 13:08:42 +0300

    add slide switch logic and current components

--- src/components/ProductSlide.jsx
+++ src/components/ProductSlide.jsx
@@ -1,16 +1,67 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 import bottle from '../images/product_page/bottle.png';
+import plate from '../images/product_page/plate.png';
+import notebook from '../images/product_page/notebook.png';
+import plus from '../images/product_page/plus.png';
 import Advantages from './Advantages';
 
 const ProductSlide = () => {
-  // const [isClicked, setClicked] = useState(false);
+  const [isClicked, setClicked] = useState(false);
+
+  const handleClick = () => setClicked(true);
+  const modalClick = () => setClicked(false);
+
+  const innerContent = (
+    <div className='product-container'>
+      <div className='product-innner-container'>
+        <div className='product-row-1'>
+          <p className='product-header'>КЛЮЧЕВОЕ СООБЩЕНИЕ</p>
+          <h1 className='product-subheader'>
+            BREND<span>XY</span>
+          </h1>
+        </div>
+        <div className='product-row-2'>
+          <div className='product-col-1'>
+            <div className='product-card-container-1'>
+              <div className='product-card-1' />
+              <div className='product-card-inner'>
+                <img src={plate} alt='plate logo' className='icon' />
+                <p className='text'>
+                  Ehicula ipsum a arcu cursus vitae. Eu non diam phasellus
+                  vestibulum lorem sed risus ultricies
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className='product-col-2'>
+            <div className='product-card-container-2'>
+              <div className='product-card-2' />
+              <div className='product-card-inner-2'>
+                <img src={notebook} alt='notebook' className='icon' />
+                <p className='text'>A arcu cursus vitae</p>
+              </div>
+            </div>
+            <button className='btn' onClick={handleClick}>
+              <img className='btn-logo' src={plus} alt='plus button' />
+              <p className='btn-text'>Подробнее</p>
+            </button>
+          </div>
+          <div className='product-col-2'></div>
+        </div>
+      </div>
+    </div>
+  );
+
+  const currentComponent = isClicked ? (
+    <Advantages modalClick={modalClick} />
+  ) : (
+    innerContent
+  );
 
   return (
     <>
       <img className='product-bottle' src={bottle} alt='bottle' />
-      <div className='product-main-container'>
-        <Advantages />
-      </div>
+      <div className='product-main-container'>{currentComponent}</div>
     </>
   );
 };
