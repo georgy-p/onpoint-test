6aaaec13c26884e9f0a02b4ff9b7cd8fc729c306
George <81578359+georgy-p@users.noreply.github.com>
2023-04-26 19:34:00 +0300

    add images

--- src/components/MainSlide.jsx
+++ src/components/MainSlide.jsx
@@ -1,4 +1,13 @@
 import nextbtn from '../images/buttons/nextbtn.png';
+import pinkSperm1 from '../images/main/pink_sperm_1.png';
+import bakti2 from '../images/main/animated/bakti_2.png';
+import bakti3 from '../images/main/animated/bakti_3.png';
+import bakti1 from '../images/main/animated/bakti_1.png';
+import bakti4 from '../images/main/animated/bakti_4.png';
+import stick from '../images/main/stick.png';
+import unfocusSperm from '../images/main/unfocus_sperm_.png';
+import unfocusBakti from '../images/main/unfocus_bakti.png';
+import unfocusBakti1 from '../images/main/unfocus_bakti_1.png';
 
 const Main = ({ goToTextSlide }) => {
   return (
@@ -21,6 +30,27 @@ const Main = ({ goToTextSlide }) => {
           </div>
         </div>
       </div>
+      <img src={pinkSperm1} alt='pinkSperm1' className='pinkSperm1 fixed' />
+      <img src={bakti2} alt='bakti2' className='bakti2 animate' />
+      <img src={bakti3} alt='bakti3' className='bakti3 animate' />
+      <img src={bakti1} alt='bakti3' className='bakti1 animate' />
+      <img src={bakti4} alt='bakti3' className='bakti4 animate' />
+      <img src={stick} alt='stick' className='stick fixed' />
+      <img
+        src={unfocusSperm}
+        alt='unfocusSperm'
+        className='unfocusSperm fixed'
+      />
+      <img
+        src={unfocusBakti}
+        alt='unfocusBakti'
+        className='unfocusBakti fixed'
+      />
+      <img
+        src={unfocusBakti1}
+        alt='unfocusBakti1'
+        className='unfocusBakti1 fixed'
+      />
     </div>
   );
 };
