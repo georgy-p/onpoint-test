9f422fe90e02f57d74c150960f9eadaeb45ceb90
George <81578359+georgy-p@users.noreply.github.com>
2023-04-26 19:35:42 +0300

    add props for modal window control

--- src/components/product/ProductSlide.jsx
+++ src/components/product/ProductSlide.jsx
@@ -1,20 +1,26 @@
-import { useState } from 'react';
 import bottle from '../../images/product_page/bottle.png';
 import plate from '../../images/product_page/plate.png';
 import notebook from '../../images/product_page/notebook.png';
 import plus from '../../images/product_page/plus.png';
+import bubble1 from '../../images/product_page/animated/bubble1.png';
+import bubble2 from '../../images/product_page/animated/bubble2.png';
+import bubble3 from '../../images/product_page/animated/bubble3.png';
+import bubble4 from '../../images/product_page/animated/bubble4.png';
+import bubble5 from '../../images/product_page/animated/bubble5.png';
+import bubble6 from '../../images/product_page/animated/bubble6.png';
+import bubble7 from '../../images/product_page/animated/bubble7.png';
+import bubble8 from '../../images/product_page/animated/bubble8.png';
+
 import Overlay from './Overlay';
 import Advantages from './Advantages';
 
-const ProductSlide = () => {
-  const [isClicked, setClicked] = useState(false);
-
+const ProductSlide = ({ isClickedModal, setClickedModal }) => {
   const handleClick = () => {
-    setClicked(true);
+    setClickedModal(true);
   };
 
   const modalClick = () => {
-    setClicked(false);
+    setClickedModal(false);
   };
 
   const innerContent = (
@@ -58,7 +64,7 @@ const ProductSlide = () => {
     </div>
   );
 
-  const currentComponent = isClicked ? (
+  const currentComponent = isClickedModal ? (
     <Advantages modalClick={modalClick} />
   ) : (
     innerContent
@@ -67,8 +73,16 @@ const ProductSlide = () => {
   return (
     <div className='product'>
       <img className='product-bottle' src={bottle} alt='bottle' />
+      <img src={bubble1} alt='bubble1' className='bubble1 absolute' />
+      <img src={bubble2} alt='bubble2' className='bubble2 absolute' />
+      <img src={bubble3} alt='bubble4' className='bubble3 absolute' />
+      <img src={bubble4} alt='bubble4' className='bubble4 absolute' />
+      <img src={bubble5} alt='bubble5' className='bubble5 absolute' />
+      <img src={bubble6} alt='bubble6' className='bubble6 absolute' />
+      <img src={bubble7} alt='bubble7' className='bubble7 absolute' />
+      <img src={bubble8} alt='bubble8' className='bubble8 absolute' />
       <div className='product-main-container'>{currentComponent}</div>
-      <Overlay isClicked={isClicked}></Overlay>
+      <Overlay isClicked={isClickedModal}></Overlay>
     </div>
   );
 };
